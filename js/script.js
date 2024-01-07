3bcb0f6677566861412545b1de9d1c3ca115f963
bassemadelmohamed <bassemadel6@gmail.com>
2024-01-07 13:48:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+var siteName = document.getElementById("siteName");
+
+var bookmarkURL = document.getElementById("bookmarkURL");
+
+if(localStorage.getItem('list') !== null){
+    siteList = JSON.parse(localStorage.getItem('list')) ;
+    display();
+}
+
+var siteList = [];
+
+function addSite(){
+
+var site = {
+    name : siteName.value,
+    URL : bookmarkURL.value,
+}
+siteList.push (site)
+localStorage.setItem('list', JSON.stringify(siteList))
+clear();
+display();
+console.log( siteList );
+}
+
+function clear(){
+    siteName.value = "";
+    bookmarkURL.value= "";
+}
+
+function display(){
+    var box = "";
+    for ( var i=0 ; i<siteList.length ; i++){
+         box +=`<tr>
+        <td> ${i+1} </td>
+        <td> ${siteList[i].name} </td>
+        <td>
+        <a href=" ${siteList[i].URL}"> <button " class="btn btn-success btn-visit pe-3">
+            <i class="fa-solid fa-eye"></i>
+            Visit
+        </button> 
+        </a>
+        </td>
+        <td>
+        <button onclick=" deleteItem( ${i}) " class="btn btn-danger btn-delete pe-2">
+        <i class="fa-solid fa-trash-can"></i>
+        Delete
+        </button>
+        </td>
+        </tr>
+        ` 
+    }
+document.getElementById('tableContent').innerHTML= box
+
+}
+
+function deleteItem( index ){
+    siteList.splice( index , 1);
+    localStorage.setItem('list',JSON.stringify(siteList))
+    console.log(siteList);
+    display();
+
+}
+
+
+// function visitSite(){
+// alert(bookmarkURL.value)
+// // display()
+// }
+
+// function visitSite(URL) {
+// //     // You can replace 'https://example.com' with the desired URL
+// //     // var websiteUrl = bookmarkURL.value ;
+// //     window.location.href= URL;
+
+// //     // Open the URL in a new tab/window
+// //     // window.open(websiteUrl, '_blank');
+// // }
